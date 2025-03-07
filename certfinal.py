7a30719ea7e152bc78fe79ad9e70d456cd272366
WHooper03 <wmhooper03@gmail.com>
2025-03-07 15:19:00 -0500

    Add files via upload

---
+++ certfinal.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Toools and Ecosystem are summarized.
+
+# **Objectives:**
+# -Listing popular data science languages
+# -Listing common libraries
+# -Listing data science tools
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. SQL
+# 3. R
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. ggplot
+# 2. dyplyr
+# 3. TensorFlow
+
+# |Data Science Tools|
+# -----------------------------
+# |ApacheHadoop|
+# |RStudio|
+# |ApacheSpark|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+#This is a simple arithmetic expression to multiply then add integers
+(3*4)+5
+
+
+# In[6]:
+
+
+#This will convert 200 minutes to hours by dividing by 60
+200/60
+
+
+# ## Author
+# Max Hooper
